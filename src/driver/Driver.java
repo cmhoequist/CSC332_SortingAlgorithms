11b7586866e1ec8b5085e0e81f4c9648944facb7
jessicabailey <jmb1323@jagmail.southalabama.edu>
2016-10-17 13:10:51 -0500

    Added buttons and worked on GUI layout

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -6,6 +6,10 @@ import java.util.*;
  * Created by Moritz on 10/5/2016.
  */
 public class Driver {
+    public class arraySizer {
+        int arraySize;
+    }
+
     private static Integer[] sampleData = {100, 2, 6, 4, 5, 15, 11, 10, 9, 8, 7, 2, 14};
 
     public static void main(String[] args){
