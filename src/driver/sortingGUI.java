9b164a6a9d87d9ffee9f7f2e68af47f1ce798a21
jessicabailey <jmb1323@jagmail.southalabama.edu>
2016-10-17 11:05:46 -0500

    Added buttons and worked on GUI layout

--- src/driver/sortingGUI.java
+++ src/driver/sortingGUI.java
@@ -26,15 +26,29 @@ public class sortingGUI extends Driver{
     public static void main (String[] args){
         //JOptionPane.showMessageDialog(null, "Mergesort Algorithm GUI");
         GUI_Display displayPanel = new GUI_Display();
+        GridLayout GUI_buttons = new GridLayout(0,10);
         JButton doneButton = new JButton("Done");
         ButtonHandler listener = new ButtonHandler();
         doneButton.addActionListener(listener);
+        JLabel num_label = new JLabel("Choose a number");
 
 
         JPanel content = new JPanel();
+        JPanel buttons = new JPanel();
         content.setLayout(new BorderLayout());
         content.add(displayPanel, BorderLayout.CENTER);
         content.add(doneButton, BorderLayout.SOUTH);
+        content.add(buttons, BorderLayout.NORTH);
+        buttons.setLayout(GUI_buttons);
+        buttons.add(new JButton("1"));
+        buttons.add(new JButton("2"));
+        buttons.add(new JButton("3"));
+        buttons.add(new JButton("4"));
+        buttons.add(new JButton("5"));
+        buttons.add(new JButton("6"));
+        buttons.add(new JButton("7"));
+        buttons.add(new JButton("8"));
+        buttons.add(new JButton("9"));
 
         JFrame window = new JFrame("Mergesort");
         window.setContentPane(content);
