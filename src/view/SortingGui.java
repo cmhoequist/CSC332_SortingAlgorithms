d14e5c80ea3c1c810c34271e78fab45c7b92d362
Moritz <cmhoequist@gmail.com>
2016-10-18 11:05:32 -0500

    Simplified gui code

--- src/view/SortingGui.java
+++ src/view/SortingGui.java
@@ -1,10 +1,8 @@
 package view;
 
-import driver.Driver;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,21 +12,7 @@ import java.util.List;
  *
  * Interface between the user and the mergesort algorithm.
  */
-public class SortingGui extends Driver {
-
-    private static class GUI_Display extends JPanel {
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawString("Mergesort Algorithm GUI", 20, 30);
-        }
-
-    }
-
-    private static class ButtonHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            System.exit(0);
-        }
-    }
+public class SortingGui extends JFrame {
 
     private void rangeErr(Double num){
         JOptionPane.showMessageDialog(null, num + " is not in range.\nPlease enter a list of numbers in the range [1,9]", "Value out of Range", JOptionPane.WARNING_MESSAGE);
@@ -37,6 +21,9 @@ public class SortingGui extends Driver {
     private List<Double> getInput(){
         List<Double> outcomes = new LinkedList<>();
         String inputString = (String)JOptionPane.showInputDialog(null, "Enter a space-delimited list of numbers in the range [1,9]","Input Dialog", JOptionPane.QUESTION_MESSAGE, null, null, "1 2 3");
+        if(inputString == null){ //Returns null on cancel
+            return new LinkedList<>();
+        }
         String[] elements = inputString.split(" ");
         for(String element : elements){
             Double num = Double.parseDouble(element);
@@ -53,42 +40,30 @@ public class SortingGui extends Driver {
     }
 
     public SortingGui(){
+        String[] buttonLabels = {"1","2","3","4","5","6","7","8","9"};
+        List<JButton> buttonList = new ArrayList<>();
+        for(String label : buttonLabels){
+            buttonList.add(new JButton(label));
+        }
         JButton inputButton = new JButton("Input");
         inputButton.addActionListener((event) -> getInput());
-        //JOptionPane.showMessageDialog(null, "Mergesort Algorithm GUI");
-        GUI_Display displayPanel = new GUI_Display();
-        GridLayout GUI_buttons = new GridLayout(0,10);
         JButton doneButton = new JButton("Done");
-        ButtonHandler listener = new ButtonHandler();
-        doneButton.addActionListener(listener);
-        JLabel num_label = new JLabel("Choose a number");
-        displayPanel.add(num_label);
+        doneButton.addActionListener((event) -> dispose());
 
+        JPanel contentPanel = new JPanel();
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new GridLayout(3,3));
+            buttonList.forEach(buttonPanel::add);
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
+        contentPanel.add(inputButton);
+        contentPanel.add(buttonPanel);
+        contentPanel.add(doneButton);
 
-        JPanel content = new JPanel();
-        JPanel buttons = new JPanel();
-        content.setLayout(new BorderLayout());
-        content.add(displayPanel, BorderLayout.CENTER);
-        content.add(doneButton, BorderLayout.SOUTH);
-        content.add(buttons, BorderLayout.NORTH);
-        buttons.setLayout(GUI_buttons);
-        buttons.add(new JButton("1"));
-        buttons.add(new JButton("2"));
-        buttons.add(new JButton("3"));
-        buttons.add(new JButton("4"));
-        buttons.add(new JButton("5"));
-        buttons.add(new JButton("6"));
-        buttons.add(new JButton("7"));
-        buttons.add(new JButton("8"));
-        buttons.add(new JButton("9"));
-        content.add(inputButton);
+        add(contentPanel);
 
-        JFrame window = new JFrame("Mergesort");
-        window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        window.setContentPane(content);
-        window.setSize(600, 500);
-        window.setLocation(100, 100);
-        window.setVisible(true);
-
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(600, 500);
+        setLocation(100, 100);
+        setVisible(true);
     }
 }
