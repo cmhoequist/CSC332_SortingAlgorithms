1bfc171c7f261f9afdc5e268e8e4e15509d6ed20
Moritz <cmhoequist@gmail.com>
2016-10-25 08:18:14 -0500

    -Removed unused imports

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,13 +1,6 @@
 package driver;
 
-import sorter.MergeSort;
 import view.SortingGui;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created: 10/5/2016
